20d03bc196b1f1cd878b0915b7fced89a813d349
AyonC <e10901ayon@dpsrkp.net>
2022-06-30 22:17:29 +0530

    Complete Project

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log(process.args);
+const getNation = async (name) => {
+  try {
+    const data = await (
+      await fetch(`https://api.nationalize.io?name=${name}`)
+    ).json();
+    for (let i = 0; i < data.country.length; i++) {
+      console.log(
+        `Country: ${data.country[i].country_id}, Probability: ${data.country[i].probability} \n`
+      );
+    }
+  } catch (err) {
+    console.log(`unspecified "name" arg`);
+  }
+};
+
+getNation(process.argv.slice(2).toString());
